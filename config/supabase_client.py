b8bd12ee93f05df0d3bfe57195400f28052c6c71
abdulrahman ahmed alkholaifi <128901809+abooda7m@users.noreply.github.com>
2025-07-14 21:39:05 +0300

    Update supabase_client.py

--- config/supabase_client.py
+++ config/supabase_client.py
@@ -3,7 +3,7 @@ from supabase import create_client, Client
 
 
 
-url = "https://bwcrzsxdufasjwvrasxp.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3Y3J6c3hkdWZhc2p3dnJhc3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMzIxMTUsImV4cCI6MjA2MzcwODExNX0.WbaNQ_GTnxmc3fLyI-t0hRTA7lGE5g3y-LUSnKWbgmk"
+url = "https://****"
+key = "Key"
 supabase: Client = create_client(url, key)
 TABLE_NAME = "superstore_sales"
